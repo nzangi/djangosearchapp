8a2809347d6e8f7d9f6a01d3d8be8ff486da45c9
nzangi <nzangimuoki284@gmail.com>
2024-01-17 15:43:08 +0300

    working on sign in

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,10 @@ from .forms import SignInForm
 app_name = 'users'
 urlpatterns = [
     path('signup/',views.signup,name='signup'),
-    path('signin/',auth_views.LoginView.as_view(template_name='users/signin.html',authentication_form=SignInForm),name='signin'),
-    path('signout/', auth_views.LogoutView.as_view(http_method_names=['post']),name='signout'),
-# template_name='users/signout.html'
+    # path('signin/',auth_views.LoginView.as_view(template_name='users/signin.html',authentication_form=SignInForm),name='signin'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout, name='logout'),
+
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LogoutView
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from .forms import SignUpForm,SignInForm
+from django.contrib.auth import logout, authenticate, login
 
 
 # Create your views here.
@@ -17,3 +18,37 @@ def signup(request):
         'form': form,
     })
 
+
+def signin(request):
+    if request.method == "POST":
+        print("if started working on form")
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            print(username)
+            print(password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request,
+                              'users/signin.html',
+                              {'form': form, 'error_message': 'Invalid username or password'})
+        else:
+            print("The form is not Valid")
+            print(form.errors)
+            # return render(request, 'users/signin.html', {'form': form, 'form_errors': form.errors})
+    else:
+        form = SignInForm()
+
+    return render(request,'users/signin.html',{
+        'form':form,
+    })
+
+
+
+
+def logout_view(request):
+    logout(request)
