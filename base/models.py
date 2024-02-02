d3d15a52dab7650fa08767d3d9212ae5fa39c367
nzangi <nzangimuoki284@gmail.com>
2024-02-02 16:24:13 +0300

    modes in django

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UploadFileModel(models.Model):
+    pdf_file=models.FileField(upload_to='media/')
+    pdf_user = models.ForeignKey(User,on_delete=models.CASCADE)
+    class Meta:
+            db_table = "uploadfiles"
+
+    def __str__(self):
+        return self.pdf_file.name
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from .forms import UploadFileForm
 
 
 # Create your views here.
@@ -8,7 +9,16 @@ from django.shortcuts import render, redirect
 #     return render(request, 'base/base.html')
 @login_required
 def upload_pdf(request):
-    return render(request, 'base/upload_pdf.html')
+    if request.method=="POST":
+        form = UploadFileForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            form = UploadFileForm()
+    else:
+        form = UploadFileForm()
+
+    return render(request, 'base/upload_pdf.html',{'form':form})
 
 def view_pdf(request):
     return render(request, 'base/view_pdfs.html')
